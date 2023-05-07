aa0102106ae9b410d9df9c9cb1bd746b3dcb4726
AwesomeHye <45376682+AwesomeHye@users.noreply.github.com>
2023-05-07 12:16:58 +0900

    [DEV] work1

--- src/main/java/dev/elsboo/gitpractice/GitPracticeApplication.java
+++ src/main/java/dev/elsboo/gitpractice/GitPracticeApplication.java
@@ -1,13 +1,16 @@
 package dev.elsboo.gitpractice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class GitPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitPracticeApplication.class, args);
+        log.info("dev work1");
     }
 
 }
